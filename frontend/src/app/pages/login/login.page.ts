de08e1ea57afd13928e350cd45070ba0a563aa14
irfan-rumon <irfannisho8571@gmail.com>
2022-10-17 17:31:37 +0600

    Fix: emptying input filed

--- frontend/src/app/pages/login/login.page.ts
+++ frontend/src/app/pages/login/login.page.ts
@@ -1,18 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { LoggerUser } from 'src/app/models/loggerUser';
 import { AuthorizationService } from 'src/app/servicesApi/authorization.service';
 import { UserApiService } from 'src/app/servicesApi/user-api.service';
 import { Router } from '@angular/router';
 import { Location } from '@angular/common';
 
+
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage implements OnInit, OnDestroy {
 
+ 
   user: LoggerUser = {} as LoggerUser;
+
+   failedLogin:boolean = false;
 
   constructor(private auth:  AuthorizationService,
               private userApi: UserApiService,
@@ -20,18 +25,32 @@ export class LoginPage implements OnInit {
               private router: Router) { }
  
   ngOnInit() {
-   
-   
+    this.user.email = '';
+    this.user.password = '';
+    this.failedLogin= false;
+  }
+
+  ngOnDestroy(){
+    this.user.email = '';
+    this.user.password = '';
+    this.failedLogin= false;
+  }
+  ngOnViewChange(){
+    this.user.email = '';
+    this.user.password = '';
+    this.failedLogin= false; 
   }
  
   onLogin(){
   
       this.user.strategy = "local";
       this.userApi.logUser(this.user).subscribe(   (response)=>{   
-          this.auth.setToken(response["accessToken"]);        
+          this.auth.setToken(response["accessToken"]);    
+          this.user.email = '';
+          this.user.password = '';  
           this.router.navigate(['/home']);                               
       }, (err)=>{
-         console.log("Error!!!");
+         this.failedLogin = true;
       })
   }
 
